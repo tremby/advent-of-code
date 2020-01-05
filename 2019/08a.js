527a8e19e44eb514566c07aece6b56fea89301c8
Bart Nagel <bart@tremby.net>
2020-01-04 16:08:22 -0800

    Refactor day 8 puzzle 1 a little for puzzle 2

--- 2019/08a.js
+++ 2019/08a.js
@@ -9,26 +9,37 @@ function stringToPixels(string) {
 		.map(num => parseInt(num, 10))
 }
 
-function pixelsToLayers(pixels, width, height) {
-	const copy = [...pixels]
-	const pixelsPerLayer = width * height
-	const layers = []
-	for (let pos = 0; pos < copy.length; pos += pixelsPerLayer) {
-		if (pos + pixelsPerLayer > copy.length) {
-			throw new Error(`Incomplete layer: ${copy.length - pos} extra pixels`)
+function splitArray(array, width) {
+	const out = []
+	for (let pos = 0; pos < array.length; pos += width) {
+		if (pos + width > array.length) {
+			throw new Error(`Not enough data: ${array.length - pos} extra items`)
 		}
-		layers.push(copy.slice(pos, pos + pixelsPerLayer))
+		out.push(array.slice(pos, pos + width))
 	}
-	return layers
+	return out
+}
+
+function pixelsToLayers(pixels, width, height) {
+	return splitArray(pixels, width * height)
 }
 
 function getValueCount(value, pixels) {
 	return pixels.filter(pixel => pixel === value).length
 }
 
+function getInput() {
+	return fs.readFileSync('08.in', { encoding: 'utf8' })
+}
+
 if (require.main === module) {
 	assert.deepEqual(stringToPixels(''), [])
 	assert.deepEqual(stringToPixels('1234'), [1,2,3,4])
+
+	assert.deepEqual(splitArray([0,1], 1), [[0], [1]])
+	assert.deepEqual(splitArray([0,1], 2), [[0,1]])
+	assert.deepEqual(splitArray([0,1,2,3,4,5], 2), [[0,1], [2,3], [4,5]])
+	assert.throws(() => splitArray([0,1,2,3,4], 4))
 
 	assert.deepEqual(pixelsToLayers([0,1,2,3,4,5], 3, 2), [[0,1,2,3,4,5]])
 	assert.deepEqual(pixelsToLayers([1,2,3,4,5,6,7,8,9,0,1,2], 3, 2), [[1,2,3,4,5,6],[7,8,9,0,1,2]])
@@ -42,7 +53,7 @@ if (require.main === module) {
 	assert.equal(getValueCount(0, [0,1,0]), 2)
 	assert.equal(getValueCount(0, [1,0,1]), 1)
 
-	const pixels = stringToPixels(fs.readFileSync('08.in', { encoding: 'utf8' }))
+	const pixels = stringToPixels(getInput())
 	const layers = pixelsToLayers(pixels, 25, 6)
 	const bestLayer = layers
 		.reduce((best, layer) => {
